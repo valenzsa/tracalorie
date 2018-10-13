15f96e29c92e8ee5e8e2e3d1f2a3a3ab168a7370
Santino Valenzuela <s0nnyv123@gmail.com>
2018-10-13 13:02:43 -0400

    Delete and Clear from localStorage

--- app.js
+++ app.js
@@ -50,6 +50,31 @@ const StorageCtrl = (function() {
         items = JSON.parse(localStorage.getItem("items"));
       }
       return items;
+    },
+    updatedItemStorage: function(updatedItem) {
+      let items = JSON.parse(localStorage.getItem("items"));
+
+      items.forEach(function(item, index) {
+        if (updatedItem.id === item.id) {
+          items.splice(index, 1, updatedItem);
+        }
+      });
+
+      localStorage.setItem("items", JSON.stringify(items));
+    },
+    deleteItemFromStorage: function(id) {
+      let items = JSON.parse(localStorage.getItem("items"));
+
+      items.forEach(function(item, index) {
+        if (id === item.id) {
+          items.splice(index, 1);
+        }
+      });
+
+      localStorage.setItem("items", JSON.stringify(items));
+    },
+    clearItemsFromStorage: function() {
+      localStorage.removeItem("items");
     }
   };
 })();
@@ -444,6 +469,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // Update localStorage
+    StorageCtrl.updatedItemStorage(updatedItem);
+
     UICtrl.clearEditState();
   };
 
@@ -465,6 +493,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // Delete from localStorage
+    StorageCtrl.deleteItemFromStorage(currentItem.id);
+
     UICtrl.clearEditState();
   };
 
@@ -480,6 +511,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
     // Remove from UI
     UICtrl.removeItems();
+
+    // Clear from localStorage
+    StorageCtrl.clearItemsFromStorage();
 
     // Hide UL
     UICtrl.hideList();
